b299e165f43481555c63ba301d7a8cb4c41951ee
Caio R. S. Araújo <159500703+silvaraujo@users.noreply.github.com>
2024-04-03 23:49:07 -0300

    Update Automac_Parte2.js

--- Automac_Parte2.js
+++ Automac_Parte2.js
@@ -9,3 +9,12 @@ if (ticketopen) {
     console.log("Ticket não encontrado.");
 }
 
+// Selecionando o elemento correspondente ao item "00. Avaliação Sem Comentário"
+var avaliacaoSemComentarioItem = document.querySelector(".item-list.tooltipster.ng-scope.blank:nth-of-type(1)");
+
+// Simulando o clique no elemento
+if (avaliacaoSemComentarioItem) {
+    avaliacaoSemComentarioItem.click();
+} else {
+    console.log("O elemento '00. Avaliação Sem Comentário' não foi encontrado.");
+}
