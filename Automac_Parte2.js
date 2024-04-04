73636de30480af327e6e70f132c2b15af4017942
Caio R. S. Araújo <159500703+silvaraujo@users.noreply.github.com>
2024-04-04 14:05:22 -0300

    Update Automac_Parte2.js

--- Automac_Parte2.js
+++ Automac_Parte2.js
@@ -57,3 +57,30 @@ setTimeout(() => {
         console.log('00. Avaliação Sem Comentário - preenchido');
     }
 }, 4000); // Intervalo de 4 segundos antes de clicar no último elemento
+
+    // Seleciona a nota de avaliação do ticket
+    var Primeiranota = document.querySelector('span.review-rating.ng-binding.ng-scope');
+    if (Primeiranota) {
+        var Notaticket = Primeiranota.textContent; // Obtém a nota do ticket
+        console.log("A nota da avaliação é:", Notaticket); // Exibe a nota do ticket
+        var NotaDoTicket = "Nota " + Notaticket; // Concatena a nota com a string "Nota"
+    } else {
+        console.log("Nota não encontrada"); // Nota de avaliação não encontrada
+    }
+// Localizando todas as labels com data-ng-class="tag.selection" e classe "ng-binding blank"
+var labels = document.querySelectorAll("label[data-ng-class='tag.selection'].ng-binding.blank");
+
+// Iterando sobre todas as labels encontradas
+labels.forEach(function(label) {
+    // Obtendo o conteúdo da label
+    var content = label.textContent.trim();
+    // Verificando se o conteúdo da label é igual à variável notaticket
+    if (content === NotaDoTicket) {
+        // Preenchendo o input com o valor da variável notaticket
+        var input = document.querySelector("#modal-show-ticket > div.ticket-container > div > div.ticket-options > div.input-container.relative > div.ticket-tags.actions-item.ng-scope > ul > input");
+        input.value = NotaDoTicket;
+        console.log("Preenchendo com", NotaDoTicket);
+        // Simulando um clique na label
+        label.click();
+    }
+});
