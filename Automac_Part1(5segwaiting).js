b344d8d25fc3b7faf2719cca11876e42eb7c98e3
Caio R. S. Araújo <159500703+silvaraujo@users.noreply.github.com>
2024-04-05 14:56:15 -0300

    Update Automac_Part1(5segwaiting).js
    parâmetros de segurança

--- Automac_Part1(5segwaiting).js
+++ Automac_Part1(5segwaiting).js
@@ -1,4 +1,3 @@
-// Armazena a marca
 setTimeout(() => {
     // Procura o elemento HTML que contém as iniciais da marca
     const brandSpan = document.querySelector('span[data-ng-bind="::brandInitials(post)"]');
@@ -12,17 +11,19 @@ setTimeout(() => {
             console.log("Esse ticket é do Ponto"); // Marca não é Casas Bahia
         }
     } else {
-        console.log("Marca não encontrada"); // Iniciais da marca não encontradas
+        console.error("Marca não encontrada"); // Iniciais da marca não encontradas
+        return;
     }
 
-    // Clica no botão "Abrir Ticket" (se existir)
+    // Clica no botão "Abrir Ticket"
     setTimeout(() => {
         const newTicketButton = document.querySelector('a.new-ticket[data-ng-if="displayNewTicketButton(post)"]');
         if (newTicketButton) {
             newTicketButton.click(); // Simula um clique no botão "Abrir Ticket"
             console.log("Ticket aberto"); // Confirmação de que o ticket foi aberto
         } else {
-            console.log("Botão para abrir novo ticket não encontrado com o seletor."); // Botão "Abrir Ticket" não encontrado
+            console.error("Botão para abrir novo ticket não encontrado com o seletor.");
+            return;
         }
 
         // Função para preencher a marca do ticket
@@ -33,29 +34,31 @@ setTimeout(() => {
                     selectElement.value = marca; // Define o valor da marca no select
                     selectElement.dispatchEvent(new Event('change')); // Simula um evento de mudança para o select
                 } else {
-                    console.log("Elemento da marca não encontrado com o ID."); // Elemento select para a marca não encontrado
+                    console.log("Elemento da marca não encontrado com o ID.");
+                    return;
                 }
             }
-
+//-------------Lembrar de colocar um if else aqui, por segurança, visto que não se sabe se o código deles falham o enviam vázio na hora de enviar o Json
             // Preenche a marca do ticket com base nas iniciais da marca
             setTimeout(() => {
                 if (brandValue === 'C') {
                     preencherMarca("616038478607cd31975c4006_casas_bahia"); // Preenche a marca como Casas Bahia
-                    console.log("Marca preenchida CB"); // Confirmação de que a marca foi preenchida como Casas Bahia
+                    console.log("Marca preenchida como CB"); // Confirmação de que a marca foi preenchida como Casas Bahia
                 } else {
                     preencherMarca("616038478607cd31975c4006_ponto"); // Preenche a marca como Ponto
-                    console.log("Marca preenchida PIN"); // Confirmação de que a marca foi preenchida como Ponto
+                    console.log("Marca preenchida como PIN"); // Confirmação de que a marca foi preenchida como Ponto
                 }
-
+//--------------------------------------------------------------------------------------------------------------------------------------------
                 // Função para preencher o responsável do ticket
-                setTimeout(() => {
+                setTimeout(() => {   //verificar se é mesmo necessário esse setTimeout
                     function preencherResponsavel(nomeResponsavel) {
                         const selectElement = document.querySelector("select.responsible-select"); // Encontra o select para o responsável
                         if (selectElement) {
                             selectElement.value = nomeResponsavel; // Define o valor do responsável no select
                             selectElement.dispatchEvent(new Event('change')); // Simula um evento de mudança para o select
                         } else {
-                            console.log("Elemento do responsável não encontrado com o selector."); // Elemento select para o responsável não encontrado
+                            console.error("Elemento do responsável não encontrado.");
+                            return;
                         }
                     }
 
@@ -74,7 +77,8 @@ setTimeout(() => {
                                 element1.click(); // Simula um clique no elemento para enviar o formulário
                                 console.log("Json enviado!"); // Confirmação de que o JSON foi enviado
                             } else {
-                                console.log("Elemento não encontrado com o XPath fornecido."); // Elemento não encontrado com o XPath fornecido
+                                console.error("Elemento não encontrado com o XPath fornecido."); // Elemento não encontrado com o XPath fornecido
+                                return;
                             }
                         }, 3000); // Espera 3 segundos antes de enviar o formulário
                     }, 2000); // Espera 2 segundos antes de preencher o responsável
